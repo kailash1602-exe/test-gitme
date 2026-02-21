5cc0824dc8d7a9ddb6b108f1c2ed942fcce5af0b
kailash1602-exe <kailash@tejaki.com>
2026-02-21 06:13:11 +0000

    Product table add

--- src/lib/schema.sql
+++ src/lib/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE Users {
     Email VARCHAR(100) UNIQUE,
     DateOfBirth DATE
 };
+
+CREATE TABLE Products {
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(50) NOT NULL,
+    ProductPrice INT NOT NULL,
+}
